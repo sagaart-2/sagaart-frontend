03489f3a820570ff1572e4799f79626be676be3a
Elizaveta Parada <parada_ev@mail.ru>
2024-07-02 11:08:11 +0200

    add-cgarts

---
+++ src/views/product-card-page/lib/mock-data.ts
@@ -0,0 +1,57 @@
+// mock-data.ts
+export const data = [
+  { year: '2014', value: 100000 },
+  { year: '2015', value: 120000 },
+  { year: '2016', value: 150000 },
+  { year: '2017', value: 200000 },
+  { year: '2018', value: 210000 },
+  { year: '2019', value: 130000 },
+  { year: '2020', value: 90000 },
+  { year: '2021', value: 110000 },
+  { year: '2022', value: 160000 },
+  { year: '2023', value: 120000 },
+  { year: '2024', value: 190000 },
+  { year: '2025', value: 180000 },
+];
+
+export const line = [
+  {
+    year: 'Mar 14',
+    price: 100000,
+  },
+  {
+    year: 'Apr 18',
+    price: 180000,
+  },
+  {
+    year: 'Jun 20',
+    price: 190000,
+  },
+  {
+    year: 'Mar 21',
+    price: null,
+  },
+  {
+    year: 'Feb 22',
+    price: 120000,
+  },
+  {
+    year: 'Mar 23',
+    price: null,
+  },
+  {
+    year: 'Mar 24',
+    price: 210000,
+  },
+  {
+    year: 'Jun 24',
+    price: 215000,
+  },
+];
+
+export const country = [
+  { name: 'USA', value: 210000, color: 'indigo.6' },
+  { name: 'India', value: 120000, color: 'yellow.6' },
+  { name: 'Japan', value: 180000, color: 'teal.6' },
+  { name: 'Other', value: 100000, color: 'gray.6' },
+];
